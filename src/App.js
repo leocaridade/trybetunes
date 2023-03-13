b4b07b6bf7b7506b0383bbda859fcefbc34d274b
leocaridade <leocaridade@hotmail.com>
2023-03-13 16:13:32 -0400

    Resolve o requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './Components/Album';
+import Favorites from './Components/Favorites';
+import Login from './Components/Login';
+import NotFound from './Components/NotFound';
+import Profile from './Components/Profile';
+import ProfileEdit from './Components/ProfileEdit';
+import Search from './Components/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ Login }
+          />
+          <Route
+            exact
+            path="/search"
+            component={ Search }
+          />
+          <Route
+            exact
+            path="/album/:id"
+            component={ Album }
+          />
+          <Route
+            exact
+            path="/favorites"
+            component={ Favorites }
+          />
+          <Route
+            exact
+            path="/profile"
+            component={ Profile }
+          />
+          <Route
+            exact
+            path="/profile/edit"
+            component={ ProfileEdit }
+          />
+          <Route component={ NotFound } />
+        </Switch>
+      </>
+    );
   }
 }
 
---
+++ src/Components/NotFound.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <p>Page not found...</p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
