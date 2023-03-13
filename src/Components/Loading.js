2db52745a28306b156e8509d32a937415cef3693
leocaridade <leocaridade@hotmail.com>
2023-03-13 18:09:37 -0400

    Resolve o requisito 2

---
+++ src/Components/Loading.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <h1>Carregando...</h1>
+    );
+  }
+}
+
+export default Loading;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,65 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      loading: false,
+    };
+  }
+
+  handleLoginInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleBtnClick = async () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: `${name}` });
+    history.push('/search', this.state);
+  };
+
   render() {
+    const { name, loading } = this.state;
+    const minNameChar = 3;
+    const isNameValid = name.length >= minNameChar;
+
+    if (loading) {
+      return <Loading />;
+    }
+
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <input
+          type="text"
+          name="name"
+          value={ name }
+          data-testid="login-name-input"
+          onChange={ this.handleLoginInput }
+        />
+        <button
+          data-testid="login-submit-button"
+          onClick={ this.handleBtnClick }
+          disabled={ !isNameValid }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+};
 
 export default Login;
