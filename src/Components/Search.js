3faabdc0c64c7911608ed22b4d30b49801e0e6e3
leocaridade <leocaridade@hotmail.com>
2023-03-13 19:18:51 -0400

    Resolve o requisito 5

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,11 +2,45 @@ import { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artist: '',
+    };
+  }
+
+  handleArtistInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { artist } = this.state;
+    const minArtChar = 2;
+    const isArtistValid = artist.length >= minArtChar;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Page Search</p>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="artist"
+            value={ artist }
+            placeholder="Nome do Artista"
+            onChange={ this.handleArtistInput }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ !isArtistValid }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
