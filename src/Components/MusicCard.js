bffff0a017d61287f36765e10c0a1183ac2d3b70
leocaridade <leocaridade@hotmail.com>
2023-03-14 19:56:06 -0400

    Resolve o requisito 8

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,29 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loading from './Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
-function MusicCard({ songs }) {
-  const songsList = songs.slice(1).map((song) => (
-    <li key={ song.trackId }>
-      { song.trackName }
-      <audio data-testid="audio-component" src={ song.previewUrl } controls>
-        <track kind="captions" />
-        O seu navegador não suporta o elemento
-        <code>audio</code>
-      </audio>
-    </li>));
-  return (
-    <ul>
-      { songsList }
-    </ul>
-  );
+class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      songs: props.songs.slice(1).map((song) => ({ ...song, favorited: false })),
+      loading: false,
+    };
+
+    this.handleCheckboxClick = this.handleCheckboxClick.bind(this);
+  }
+
+  async handleCheckboxClick(event) {
+    this.setState({ loading: true });
+    const { songs } = this.state;
+    const track = songs.find((song) => song.trackId === Number(event.target.value));
+    await addSong(track);
+    track.favorited = true;
+    this.setState({ songs, loading: false });
+  }
+
+  render() {
+    const { songs, loading } = this.state;
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    const songsList = songs.map((song) => (
+      <li key={ song.trackId }>
+        { song.trackName }
+        <audio data-testid="audio-component" src={ song.previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+        <label>
+          Favorita
+          <input
+            type="checkbox"
+            data-testid={ `checkbox-music-${song.trackId}` }
+            value={ song.trackId }
+            checked={ song.favorited }
+            onClick={ this.handleCheckboxClick }
+          />
+        </label>
+      </li>));
+
+    return (
+      <ul>
+        { songsList }
+      </ul>
+    );
+  }
 }
 
 MusicCard.propTypes = {
   songs: PropTypes.arrayOf(
     PropTypes.shape({
-      track: PropTypes.number.isRequired,
-      trackName: PropTypes.string.isRequired,
-      previewUrl: PropTypes.string.isRequired,
+      track: PropTypes.number,
+      trackName: PropTypes.string,
+      previewUrl: PropTypes.string,
     }),
   ).isRequired,
 };
